678876d3401c6117dada76df310095a882a63c3c
fang <fang@fangs-MBP.lan>
2024-03-26 11:14:14 +0800

    init

---
+++ src/app/page.tsx
@@ -0,0 +1,33 @@
+import { CreatePost } from "~/app/_components/create-post";
+import { getServerAuthSession } from "~/server/auth";
+import { api } from "~/trpc/server";
+
+export default async function Home() {
+  // const hello = await api.post.hello({ text: "from tRPC" });
+  // const session = await getServerAuthSession();
+
+  return (
+    <main className="flex min-h-screen items-center justify-center text-3xl">
+      <p className="mb-[18vh]">Hi, I am FF.</p>
+    </main>
+  );
+}
+
+async function CrudShowcase() {
+  const session = await getServerAuthSession();
+  if (!session?.user) return null;
+
+  const latestPost = await api.post.getLatest();
+
+  return (
+    <div className="w-full max-w-xs">
+      {latestPost ? (
+        <p className="truncate">Your most recent post: {latestPost.name}</p>
+      ) : (
+        <p>You have no posts yet.</p>
+      )}
+
+      <CreatePost />
+    </div>
+  );
+}
